53af73efcc526119454fbe7abb04b7e6037ec68d
yourlass <8956451@qq.com>
2024-09-26 14:52:07 +0800

    第一次提交

---
+++ SplitText/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace SplitText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Lines = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.richTextBox1.Location = new System.Drawing.Point(1, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(433, 397);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            this.richTextBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.num);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtSP);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Lines);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(1, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 27);
+            this.panel1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(393, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Close";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(356, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 27);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Min";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(319, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 27);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Font";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Lines
+            // 
+            this.Lines.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lines.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Lines.ForeColor = System.Drawing.Color.White;
+            this.Lines.Location = new System.Drawing.Point(0, 0);
+            this.Lines.Name = "Lines";
+            this.Lines.Size = new System.Drawing.Size(67, 27);
+            this.Lines.TabIndex = 9;
+            this.Lines.Text = "Lines";
+            this.Lines.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // num
+            // 
+            this.num.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.num.Dock = System.Windows.Forms.DockStyle.Left;
+            this.num.ForeColor = System.Drawing.Color.White;
+            this.num.Location = new System.Drawing.Point(252, 0);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(67, 27);
+            this.num.TabIndex = 5;
+            this.num.Text = "num";
+            this.num.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(215, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Blank";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtSP
+            // 
+            this.txtSP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtSP.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtSP.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSP.ForeColor = System.Drawing.Color.White;
+            this.txtSP.Location = new System.Drawing.Point(178, 0);
+            this.txtSP.Name = "txtSP";
+            this.txtSP.Size = new System.Drawing.Size(37, 26);
+            this.txtSP.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(141, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Split";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(104, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "save";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(67, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Load";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(1, 25, 1, 1);
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label num;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Lines;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ SplitText/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitText
+{
+    public partial class Form1 : Form
+    {
+        string path = string.Empty;
+        public enum MouseDirection
+        {
+            Herizontal,//水平方向拖动，只改变窗体的宽度
+            Vertical,//垂直方向拖动，只改变窗体的高度
+            Declining,//倾斜方向，同时改变窗体的宽度和高度
+            None//不做标志，即不拖动窗体改变大小
+        }
+        Point mouseOff;//鼠标移动位置变量
+        bool leftFlag;//标签是否为左键
+        bool isMouseDown = false; //表示鼠标当前是否处于按下状态，初始值为否
+        MouseDirection direction = MouseDirection.None;//表示拖动的方向，起始为None，表示不拖动
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int sel = richTextBox1.SelectionStart;
+            richTextBox1.Text = richTextBox1.Text.Substring(sel);
+            richTextBox1.SaveFile(path, RichTextBoxStreamType.PlainText);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            num.Text = richTextBox1.Text.Length.ToString();
+            Lines.Text = richTextBox1.Lines.Length.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                openFile.Filter = "文本文件|*.txt|所有文件|*.*";
+                if (DialogResult.Cancel == openFile.ShowDialog())
+                {
+                    return;
+                }
+                richTextBox1.LoadFile(openFile.FileName, RichTextBoxStreamType.PlainText);
+                path = openFile.FileName;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(path, RichTextBoxStreamType.PlainText);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtSP.Text))
+            {
+                richTextBox1.Text = richTextBox1.Text.Replace(txtSP.Text, txtSP.Text + "\r\n");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in richTextBox1.Lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    sb.AppendLine(line.Trim());
+                }
+            }
+            richTextBox1.Text = sb.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fd = new FontDialog())
+            {
+                if (DialogResult.OK == fd.ShowDialog())
+                {
+                    richTextBox1.Font = fd.Font;
+                }
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //如果鼠标按下，同时有方向箭头那么直接调整大小,这里是改进的地方，不然斜角拉的过程中，会有问题
+            if (isMouseDown && direction != MouseDirection.None)
+            {
+                //设定好方向后，调用下面方法，改变窗体大小
+                ResizeWindow();
+                return;
+            }
+            //鼠标移动过程中，坐标时刻在改变
+            //当鼠标移动时横坐标距离窗体右边缘5像素以内且纵坐标距离下边缘也在5像素以内时，要将光标变为倾斜的箭头形状，同时拖拽方向direction置为MouseDirection.Declining
+            if (e.Location.X >= this.Width - 5 && e.Location.Y > this.Height - 5)
+            {
+                this.Cursor = Cursors.SizeNWSE;
+                direction = MouseDirection.Declining;
+            }
+            //当鼠标移动时横坐标距离窗体右边缘5像素以内时，要将光标变为倾斜的箭头形状，同时拖拽方向direction置为MouseDirection.Herizontal
+            else if (e.Location.X >= this.Width - 5)
+            {
+                this.Cursor = Cursors.SizeWE;
+                direction = MouseDirection.Herizontal;
+            }
+            //同理当鼠标移动时纵坐标距离窗体下边缘5像素以内时，要将光标变为倾斜的箭头形状，同时拖拽方向direction置为MouseDirection.Vertical
+            else if (e.Location.Y >= this.Height - 5)
+            {
+                this.Cursor = Cursors.SizeNS;
+                direction = MouseDirection.Vertical;
+            }
+            //否则，以外的窗体区域，鼠标星座均为单向箭头（默认） 按下时可移动界面
+            else
+            {
+                this.Cursor = Cursors.Arrow;
+                if (leftFlag)
+                {
+                    Point mouseSet = Control.MousePosition;
+                    mouseSet.Offset(mouseOff.X, mouseOff.Y); //设置移动后的位置
+                    Location = mouseSet;
+                }
+            }
+        }
+        private void ResizeWindow()
+        {
+            //这个判断很重要，只有在鼠标按下时才能拖拽改变窗体大小，如果不作判断，那么鼠标弹起和按下时，窗体都可以改变
+            if (!isMouseDown)
+                return;
+            //MousePosition的参考点是屏幕的左上角，表示鼠标当前相对于屏幕左上角的坐标this.left和this.top的参考点也是屏幕，属性MousePosition是该程序的重点
+            if (direction == MouseDirection.Declining)
+            {
+                //此行代码在mousemove事件中已经写过，在此再写一遍，并不多余，一定要写
+                this.Cursor = Cursors.SizeNWSE;
+                //下面是改变窗体宽和高的代码，不明白的可以仔细思考一下
+                this.Width = MousePosition.X - this.Left;
+                this.Height = MousePosition.Y - this.Top;
+            }
+            //以下同理
+            if (direction == MouseDirection.Herizontal)
+            {
+                this.Cursor = Cursors.SizeWE;
+                this.Width = MousePosition.X - this.Left;
+            }
+            else if (direction == MouseDirection.Vertical)
+            {
+                this.Cursor = Cursors.SizeNS;
+                this.Height = MousePosition.Y - this.Top;
+            }
+            //即使鼠标按下，但是不在窗口右和下边缘，那么也不能改变窗口大小
+            else
+                this.Cursor = Cursors.Arrow;
+        }
+        //添加窗体的MouseDown事件，并编写如下代码
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseOff = new Point(-e.X, -e.Y); //得到变量的值
+                leftFlag = true; //点击左键按下时标注为true;
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            direction = MouseDirection.None;
+            if (leftFlag)
+            {
+                leftFlag = false;//释放鼠标后标注为false;
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    this.WindowState = FormWindowState.Minimized;
+                    this.Hide();
+                    break;
+                case Keys.Q:
+                    int sel = richTextBox1.SelectionStart;
+                    richTextBox1.Text = richTextBox1.Text.Substring(sel);
+                    richTextBox1.SaveFile(path, RichTextBoxStreamType.PlainText);
+                    break;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            richTextBox1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = Screen.PrimaryScreen.WorkingArea.Width - this.Width;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+        }
+    }
+}
